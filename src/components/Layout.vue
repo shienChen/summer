5f61f46f9a5af868b43f5552b5916e21520b0c62
shienChen <1083315033@qq.com>
2021-09-18 18:27:17 +0800

    init styling and layout and router

---
+++ src/components/Layout.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="summer-layout">
+    <header>head</header>
+    <main>
+      <router-view />
+    </main>
+    <footer>foot</footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Layout",
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import "./plugins/axios";
+import "@/style/index.scss";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
